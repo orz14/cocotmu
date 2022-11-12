34f0300e579fc3ec9ff3c6279dfa1f78440c35fc
orz14 <oriezt.id@gmail.com>
2022-11-12 11:49:55 +0700

    fix btn

--- app/views/styles/style.php
+++ app/views/styles/style.php
@@ -1,2 +1,2 @@
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/orz14/cocotmu@main/public/css/style-v1.5.min.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/orz14/cocotmu@main/public/css/style-v1.6.min.css" />
     
